a9c4965d289522d574c847df7d7720b2200ca310
Mathias Decker <mathias@itgweb.de>
2018-01-13 18:20:05 +0100

    initial commit of check + a meltdown defintion

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# check_cve
+# universal CVE check for check_mk
+
+## usage
+
+place check.py into /usr/lib/check_mk_agent/local or /usr/lib/check_mk_agent/local/3600 (or any other subfolder)
+place cve directory into /usr/lib/check_mk_agent/
+
+the check will produce one single check with all checked json 50x_files
+
+## example
+
+2017-5754.json will check the installed kernel against a known (good) version which is not vulnerable against meltdown
---
+++ check_cve.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+# ref: https://stackoverflow.com/questions/11887762/compare-version-strings-in-python
+
+import glob, os, json
+from distutils.version import LooseVersion
+
+dir = "/usr/lib/check_mk_agent/cve"
+
+osname = os.popen("PATH=$PATH:/opt/puppetlabs/bin; facter operatingsystem").read().replace("\n","")
+osreleasemajor = os.popen("PATH=$PATH:/opt/puppetlabs/bin; facter operatingsystemmajrelease").read().replace("\n","")
+
+# counter
+affected = 0
+known = 0
+unknown = 0
+msg_list = []
+
+# loop cve's
+os.chdir(dir)
+for cve_file in glob.glob("*.json"):
+    cve_data = json.load(open("%s/%s" % (dir, cve_file)))
+    cve = cve_file.replace(".json","")
+    cve_name = cve_data['name']
+    try:
+        cve_info = cve_data['os'][osname]['info_url']
+        cve_query = cve_data['os'][osname]['query_command']
+        cve_package_name = cve_data['os'][osname]['package_name']
+        cve_fixed_version = cve_data['os'][osname]['versions'][osreleasemajor]['fixed_version']
+        current_version = os.popen(cve_query % cve_package_name).read().replace("\n","")
+        known += 1
+        if current_version:
+            if LooseVersion(current_version) < LooseVersion(cve_fixed_version):
+                affected += 1
+                msg_list.append("system is affected by %s (%s) - please patch %s (more info %s)" % (cve_name, cve, cve_package_name, cve_info))
+
+    except:
+        unknown += 1
+        msg_list.append("no definition found for %s and %s (%s)" % (osname, cve_name, cve))
+
+if affected > 0:
+    exit_code = 2
+    exit_status = "CRIT"
+else:
+    exit_code = 0
+    exit_status = "OK"
+
+if unknown > 0:
+    exit_code = 1
+    exit_status = "WARN"
+
+if len(msg_list) == 0:
+    msg_list.append("system looks fine - 0 of %s cve's affected" % known)
+
+print "%s check_cve known=%s|unknown=%s|affected=%s %s - %s" % (exit_code, known, unknown, affected, exit_status, ", ".join(msg_list))
